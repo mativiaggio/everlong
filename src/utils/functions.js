568b66843cbac1f1d30a72ca6d8f70ab61542d04
Matias Viaggio <matiasviaggio59@gmail.com>
2024-06-18 20:25:37 -0300

    vercel.json & getPath - v1.0.0

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "url";
+// export const getPath = (folderName) => {
+//   return path.join(process.cwd(), "src", folderName);
+// };
 export const getPath = (folderName) => {
-  return path.join(process.cwd(), "src", folderName);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.join(__dirname, "..", folderName);
 };
 
 /**
